7af679eb2d53ba74b4d1db7da0c2b16c062efc57
Nikolay <gugalohikolaychur@gmai.com>
2022-04-25 09:16:20 +0400

    code

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const http = require("http");
 const multer = require("multer");
-const upload = multer({ dest: "/uploads" });
+const upload = multer({ dest: path.join(__dirname) });
 
 const app = express();
 
